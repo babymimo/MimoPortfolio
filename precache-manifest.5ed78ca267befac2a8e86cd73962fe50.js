4334760ca0dd9cd2f3c9eccebe4e002ad1909765
babymimo <isheal@mail.com>
2019-04-21 09:08:25 +0800

    Updates

---
+++ precache-manifest.5ed78ca267befac2a8e86cd73962fe50.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f467785a4be2882d7886e34712a74af",
+    "url": "/MimoPortfolio/static/media/project1.6f467785.png"
+  },
+  {
+    "revision": "014cb81ff3c008be45a8",
+    "url": "/MimoPortfolio/static/css/main.ce9a3258.chunk.css"
+  },
+  {
+    "revision": "106fbcab6d778af6b6c5",
+    "url": "/MimoPortfolio/static/js/runtime~main.0049f679.js"
+  },
+  {
+    "revision": "4188d8d58ab0b1fbbe38",
+    "url": "/MimoPortfolio/static/js/2.47880b78.chunk.js"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/MimoPortfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "c88cc5d1881a428b050ab6257458e9de",
+    "url": "/MimoPortfolio/static/media/instagram_icon.c88cc5d1.png"
+  },
+  {
+    "revision": "014cb81ff3c008be45a8",
+    "url": "/MimoPortfolio/static/js/main.4e489d18.chunk.js"
+  },
+  {
+    "revision": "771094e26cf023b30364fea565e6321f",
+    "url": "/MimoPortfolio/static/media/project2.771094e2.png"
+  },
+  {
+    "revision": "d116739f8ebc7865d4c40ea23c562b4d",
+    "url": "/MimoPortfolio/static/media/project3.d116739f.png"
+  },
+  {
+    "revision": "c0a45cf3cd108b56bc26b5cb9e456e2b",
+    "url": "/MimoPortfolio/static/media/profile.c0a45cf3.png"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/MimoPortfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/MimoPortfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "bdab96e0311f8a00d84c720c4569bbc8",
+    "url": "/MimoPortfolio/index.html"
+  }
+];
